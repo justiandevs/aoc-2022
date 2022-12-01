f6744760623d87cbead6bfcbbadd09da9efbaac5
justiandevs <justiandev@gmail.com>
2022-12-01 14:32:59 +0100

    Day-1: Exercise-two

--- day-1/day-1.ts
+++ day-1/day-1.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 const fileContent: Array<string> = fs.readFileSync('day-1-input.txt', 'utf8').split('\n\n');
 
-const exercise = () => {
+const exerciseOne = (): number => {
   let highestTotalCalories: number = 0;
 
   for (let i in fileContent) {
@@ -21,4 +21,25 @@ const exercise = () => {
   return highestTotalCalories;
 }
 
-console.log(exercise());
+const exerciseTwo = (): number => {
+  let highestTotalCalories: number = 0;
+  let collectionOfTotalCaloriesOfEveryElf: Array<number> = [];
+
+  for (let i in fileContent) {
+    const collectionOfAllCaloriesOfOneElf: Array<string> = fileContent[i].split('\n');
+    let amountOfCaloriesOfOneElf = 0;
+
+    for (let j in collectionOfAllCaloriesOfOneElf) {
+      amountOfCaloriesOfOneElf = amountOfCaloriesOfOneElf + parseInt(collectionOfAllCaloriesOfOneElf[j]);
+    }
+
+    collectionOfTotalCaloriesOfEveryElf.push(amountOfCaloriesOfOneElf);
+  }
+
+  collectionOfTotalCaloriesOfEveryElf.sort((a, b) => a - b).slice(-3).map((topResult) => highestTotalCalories = highestTotalCalories + topResult);
+
+  return highestTotalCalories;
+}
+
+console.log('exercise-one: ' + exerciseOne());
+console.log('exercise-two: ' + exerciseTwo());
