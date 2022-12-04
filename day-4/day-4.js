98113874cfe1df2ef0a0ef84fc373681f91425c3
justiandevs <justiandev@gmail.com>
2022-12-04 12:12:16 +0100

    Day-4: Exercise two

--- day-4/day-4.js
+++ day-4/day-4.js
@@ -41,4 +41,18 @@ const exerciseOne = () => {
     });
     return amount;
 };
+const exerciseTwo = () => {
+    let amount = 0;
+    fileContent.map((line) => {
+        const transformedArray = transformArray(line);
+        if ((transformedArray[0][0] >= transformedArray[1][0] && transformedArray[0][1] <= transformedArray[1][1]) || (transformedArray[1][0] >= transformedArray[0][0] && transformedArray[1][1] <= transformedArray[0][1]) ||
+            transformedArray[0][0] == transformedArray[1][0] || transformedArray[0][0] == transformedArray[1][1] || transformedArray[0][1] == transformedArray[1][0] || transformedArray[0][1] == transformedArray[1][1] ||
+            (transformedArray[0][0] < transformedArray[1][0] && transformedArray[0][1] < transformedArray[1][1] && transformedArray[0][1] > transformedArray[1][0]) ||
+            (transformedArray[1][0] < transformedArray[0][0] && transformedArray[1][1] < transformedArray[0][1] && transformedArray[1][1] > transformedArray[0][0])) {
+            amount += 1;
+        }
+    });
+    return amount;
+};
 console.log('exercise-one: ' + exerciseOne());
+console.log('exercise-two: ' + exerciseTwo());
