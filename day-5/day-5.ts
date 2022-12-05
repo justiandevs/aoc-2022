fcb92c6f1c555e5fa233ebfc8c58aea213a2e5d2
justiandevs <justiandev@gmail.com>
2022-12-05 15:29:06 +0100

    Day-5: Exercise two

--- day-5/day-5.ts
+++ day-5/day-5.ts
@@ -6,8 +6,6 @@ const index = fileContent.indexOf('');
 const crates = fileContent.slice(0, index);
 const instructions = fileContent.slice(index + 1);
 
-const rowOfCrateNumbers = crates[index-1].split('').filter((value) => value != ' ').map((str) => parseInt(str));
-
 const instructionSets = instructions.map((instruction) => {
   const instructionSet = instruction.split('move').toString().split('from').toString().split('to').toString().split(',');
 
@@ -17,29 +15,51 @@ const instructionSets = instructions.map((instruction) => {
 const array = Array.from({ length: index }, (_, ind) =>
     crates.map((line) => line[ind * 4 + 1]).filter((char) => char != " ").filter((char) => char != `${ind + 1}`).reverse().join("")).map((line) => line.toString().split(''));
 
-const moveOneCrate = (instruction: Array<number>) => {
+const deepCopy = JSON.parse(JSON.stringify(array));
+
+const moveOneCrate = (instruction: Array<number>, arr: Array<Array<string>>) => {
   for(let i = 0; i < instruction[0]; i++) {
-    const arr = array[(instruction[1]-1)].pop()!;
-    array[(instruction[2]-1)].push(arr);
+    const arrRemovedItem = arr[(instruction[1]-1)].pop()!;
+    arr[(instruction[2]-1)].push(arrRemovedItem);
   }
 }
 
-const getTopCrates = () => {
+const moveMultipleCrates = (instruction: Array<number>, arr: Array<Array<string>>) => {
+  let finalArray = [];
+
+  for(let i = 0; i < instruction[0]; i++) {
+    const arrRemovedItem = arr[(instruction[1]-1)].pop()!;
+    finalArray.push(arrRemovedItem);
+  }
+
+  arr[(instruction[2]-1)].push(...finalArray.reverse());
+}
+
+const getTopCrates = (arr: Array<Array<string>>) => {
   let message: string = "";
 
-  array.map((list) => {
+  arr.map((list) => {
     message = message + list[list.length - 1];
   });
 
   return message;
 }
 
-const exercise = (): string => {
+const exerciseOne = (): string => {
   instructionSets.map((instruction) => {
-    moveOneCrate(instruction);
+    moveOneCrate(instruction, array);
   });
 
-  return getTopCrates();
+  return getTopCrates(array);
 }
 
-console.log('exercise-one: ' + exercise());
+const exerciseTwo = (): string => {
+  instructionSets.map((instruction) => {
+    moveMultipleCrates(instruction, deepCopy);
+  })
+
+  return getTopCrates(deepCopy);
+}
+
+console.log('exercise-one: ' + exerciseOne());
+console.log('exercise-two: ' + exerciseTwo());
