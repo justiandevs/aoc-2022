3b607df3c0e6852d03f6dcfe698ebab0de83557c
justiandevs <justiandev@gmail.com>
2022-12-07 17:37:39 +0100

    Day-7: Day 7, part 1 and 2 done

--- day-7/day-7.js
+++ day-7/day-7.js
@@ -28,33 +28,23 @@ const fileContent = fs.readFileSync('day-7-input.txt', 'utf8').split('\n');
 var commandTypes;
 (function (commandTypes) {
     commandTypes["previous"] = "..";
-    commandTypes["root"] = "/";
     commandTypes["list"] = "ls";
     commandTypes["goToDirectory"] = "cd";
 })(commandTypes || (commandTypes = {}));
-const exercise = () => {
-    let amount = 0;
+const base = () => {
     let path = [];
-    const folders = {};
-    const getSize = (initialSize, sizeToAdd) => {
-        let size = initialSize;
-        size += parseInt(String(sizeToAdd));
-        return size;
+    const tree = {};
+    const getTotalSize = (tree, dir) => {
+        const { size, folders } = tree[dir];
+        return size + folders.map((folder) => getTotalSize(tree, `${dir}/${folder}`)).reduce((a, b) => a + b, 0);
     };
     fileContent.map((line, index) => {
         var _a;
         let wholePath = path.join('/');
-        const size = ((_a = line.match(/\d+/)) === null || _a === void 0 ? void 0 : _a[0]);
-        if (size != undefined) {
-            folders[wholePath] = {
-                folders: [],
-                size: getSize(folders[wholePath].size, size),
-            };
-        }
         if (line.startsWith("$")) {
             const [, cmd, target] = line.split(" ");
             if (cmd == commandTypes.list) {
-                folders[wholePath] = {
+                tree[wholePath] = {
                     folders: [],
                     size: 0
                 };
@@ -67,12 +57,30 @@ const exercise = () => {
                     path.push(target);
                 }
             }
-            else if (cmd == commandTypes.list) {
-                //
-            }
+        }
+        if (line.startsWith('dir')) {
+            const dirName = line.split(' ');
+            tree[wholePath].folders.push(dirName[1]);
+        }
+        const size = ((_a = line.match(/\d+/)) === null || _a === void 0 ? void 0 : _a[0]);
+        if (size != undefined) {
+            tree[wholePath] = {
+                folders: tree[wholePath].folders,
+                size: tree[wholePath].size + parseInt(size),
+            };
         }
     });
-    console.log(folders);
-    return amount;
+    return Object.keys(tree).map((dir) => getTotalSize(tree, dir));
 };
-console.log('exercise-one: ' + exercise());
+const exerciseOne = () => {
+    return base().filter((amount) => amount <= 100000).reduce((a, b) => a + b, 0);
+};
+const exerciseTwo = () => {
+    const totalSpace = 70000000;
+    const neededFreeSpace = 30000000;
+    const totalDiskSpaceUsed = base().sort((a, b) => b - a)[0];
+    const spaceToFreeUp = (totalSpace - neededFreeSpace - totalDiskSpaceUsed) * -1;
+    return base().filter((amount) => amount >= spaceToFreeUp).sort((a, b) => a - b)[0];
+};
+console.log('exercise-one: ' + exerciseOne());
+console.log('exercise-two: ' + exerciseTwo());
--- day-7/day-7.ts
+++ day-7/day-7.ts
@@ -2,40 +2,27 @@ import * as fs from 'fs';
 
 const fileContent: Array<string> = fs.readFileSync('day-7-input.txt', 'utf8').split('\n');
 
-enum commandTypes { previous = "..", "root" = "/", list = "ls", goToDirectory = "cd" }
+enum commandTypes { previous = "..", list = "ls", goToDirectory = "cd" }
 
-type Folders = Record<string, { folders: [], size: number }>
+type Tree = Record<string, { folders: string[], size: number }>
 
-const exercise = (): number => {
-  let amount: number = 0;
+const base = (): number[] => {
   let path: string[] = [];
-  const folders: Folders = {};
+  const tree: Tree = {};
 
-  const getSize = (initialSize: number, sizeToAdd: string): number => {
-    let size: number = initialSize;
-
-    size += parseInt(String(sizeToAdd));
-
-    return size;
+  const getTotalSize = (tree: Tree, dir: string): number => {
+    const { size, folders } = tree[dir];
+    return size + folders.map((folder) => getTotalSize(tree, `${dir}/${folder}`)).reduce((a, b) => a + b, 0);
   }
 
   fileContent.map((line, index) => {
     let wholePath: string = path.join('/');
 
-    const size: string = (line.match(/\d+/)?.[0])!;
-
-    if(size != undefined) {
-      folders[wholePath] = {
-        folders: [],
-        size: getSize(folders[wholePath].size, size),
-      }
-    }
-
     if(line.startsWith("$")) {
       const [, cmd, target] = line.split(" ");
 
       if(cmd == commandTypes.list) {
-        folders[wholePath] = {
+        tree[wholePath] = {
           folders: [],
           size: 0
         }
@@ -47,15 +34,40 @@ const exercise = (): number => {
         } else {
           path.push(target);
         }
-      } else if(cmd == commandTypes.list) {
-        //
+      }
+    }
+
+    if (line.startsWith('dir')) {
+      const dirName = line.split(' ');
+      tree[wholePath].folders.push(dirName[1]);
+    }
+
+    const size: string = (line.match(/\d+/)?.[0])!;
+
+    if(size != undefined) {
+      tree[wholePath] = {
+        folders: tree[wholePath].folders,
+        size: tree[wholePath].size + parseInt(size),
       }
     }
   });
 
-  console.log(folders);
-
-  return amount;
+  return Object.keys(tree).map((dir) => getTotalSize(tree, dir));
 }
 
-console.log('exercise-one: ' + exercise());
+const exerciseOne = (): number => {
+  return base().filter((amount) => amount <= 100000).reduce((a, b) => a + b, 0)
+}
+
+const exerciseTwo = (): number => {
+  const totalSpace = 70000000;
+  const neededFreeSpace = 30000000;
+
+  const totalDiskSpaceUsed = base().sort((a, b) => b - a)[0];
+  const spaceToFreeUp = (totalSpace - neededFreeSpace - totalDiskSpaceUsed) * -1;
+
+  return base().filter((amount) => amount >= spaceToFreeUp).sort((a, b) => a - b)[0];
+}
+
+console.log('exercise-one: ' + exerciseOne());
+console.log('exercise-two: ' + exerciseTwo());
