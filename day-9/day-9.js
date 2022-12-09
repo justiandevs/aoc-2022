86c3b52668d83cfb199256948a326daa11396580
justiandevs <justiandev@gmail.com>
2022-12-09 11:05:51 +0100

    Day-9: Can track the movement of H

--- day-9/day-9.js
+++ day-9/day-9.js
@@ -25,6 +25,11 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const fileContent = fs.readFileSync('day-9-test-input.txt', 'utf8').split('\n');
+let yPosHead = 0;
+let xPosHead = 0;
+let yPosTail = 0;
+let xPosTail = 0;
+let visitedCoordinates = ['0,0'];
 const getInstructions = (arr) => {
     let instructions = [];
     arr.map((line) => {
@@ -32,10 +37,59 @@ const getInstructions = (arr) => {
     });
     return instructions;
 };
+const checkIfCoordinatesAreAlreadyVisited = () => {
+    if (visitedCoordinates.indexOf(`${xPosHead},${yPosHead}`) == -1) {
+        visitedCoordinates.push(`${xPosHead},${yPosHead}`);
+        return false;
+    }
+    return true;
+};
+const moveLeft = (moves) => {
+    console.log('move left', moves);
+    for (let i = 0; i < moves; i++) {
+        xPosHead -= 1;
+        checkIfCoordinatesAreAlreadyVisited();
+    }
+};
+const moveRight = (moves) => {
+    console.log('move right', moves);
+    for (let i = 0; i < moves; i++) {
+        xPosHead += 1;
+        checkIfCoordinatesAreAlreadyVisited();
+    }
+};
+const moveUp = (moves) => {
+    console.log('move up', moves);
+    for (let i = 0; i < moves; i++) {
+        yPosHead += 1;
+        checkIfCoordinatesAreAlreadyVisited();
+    }
+};
+const moveDown = (moves) => {
+    console.log('move down', moves);
+    for (let i = 0; i < moves; i++) {
+        yPosHead -= 1;
+        checkIfCoordinatesAreAlreadyVisited();
+    }
+};
 const exercise = () => {
     let amount = 0;
     const instructions = getInstructions(fileContent);
-    console.log(instructions);
+    instructions.map((instruction, index) => {
+        if (instruction[0] == "L") {
+            moveLeft(parseInt(instruction[1]));
+        }
+        else if (instruction[0] == "R") {
+            moveRight(parseInt(instruction[1]));
+        }
+        else if (instruction[0] == "U") {
+            moveUp(parseInt(instruction[1]));
+        }
+        else {
+            moveDown(parseInt(instruction[1]));
+        }
+    });
+    console.log(xPosHead, yPosHead, visitedCoordinates.length);
     return amount;
 };
 console.log('exercise: ' + exercise());
--- day-9/day-9.ts
+++ day-9/day-9.ts
@@ -2,6 +2,12 @@ import * as fs from 'fs';
 
 const fileContent: string[] = fs.readFileSync('day-9-test-input.txt', 'utf8').split('\n');
 
+let yPosHead: number = 0;
+let xPosHead: number = 0;
+let yPosTail: number = 0;
+let xPosTail: number = 0;
+let visitedCoordinates: string[] = ['0,0'];
+
 const getInstructions = (arr: string[]) => {
   let instructions: string[][] = [];
 
@@ -12,12 +18,63 @@ const getInstructions = (arr: string[]) => {
   return instructions;
 }
 
+const checkIfCoordinatesAreAlreadyVisited = (): boolean => {
+  if(visitedCoordinates.indexOf(`${xPosHead},${yPosHead}`) == -1) {
+    visitedCoordinates.push(`${xPosHead},${yPosHead}`);
+    return false;
+  } return true;
+}
+
+const moveLeft = (moves: number) => {
+  console.log('move left', moves);
+  for(let i = 0; i < moves; i++) {
+    xPosHead -= 1;
+    checkIfCoordinatesAreAlreadyVisited();
+  }
+}
+
+const moveRight = (moves: number) => {
+  console.log('move right', moves);
+  for(let i = 0; i < moves; i++) {
+    xPosHead += 1;
+    checkIfCoordinatesAreAlreadyVisited();
+  }
+}
+
+const moveUp = (moves: number) => {
+  console.log('move up', moves);
+  for(let i = 0; i < moves; i++) {
+    yPosHead += 1;
+    checkIfCoordinatesAreAlreadyVisited();
+  }
+}
+
+const moveDown = (moves: number) => {
+  console.log('move down', moves);
+  for(let i = 0; i < moves; i++) {
+    yPosHead -= 1;
+    checkIfCoordinatesAreAlreadyVisited();
+  }
+}
+
 const exercise = (): number => {
   let amount: number = 0;
 
   const instructions: string[][] = getInstructions(fileContent);
-  console.log(instructions);
 
+  instructions.map((instruction: string[], index: number) => {
+    if(instruction[0] == "L") {
+      moveLeft(parseInt(instruction[1]));
+    } else if(instruction[0] == "R") {
+      moveRight(parseInt(instruction[1]));
+    } else if(instruction[0] == "U") {
+      moveUp(parseInt(instruction[1]));
+    } else {
+      moveDown(parseInt(instruction[1]));
+    }
+  });
+
+  console.log(xPosHead, yPosHead, visitedCoordinates.length);
   return amount;
 }
 
